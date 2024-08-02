016e732b82800b03007c8c8d329a2ad4c9d44d1a
Kinoppoid <kinoppoid@gmail.com>
2024-08-02 22:07:49 +0900

    一旦動く状態にしたもの

--- compile.bat
+++ compile.bat
@@ -16,7 +16,8 @@ p2bin %TRG%.p -l 0 -r $-$
 move /Y %TRG%.bin .\file\%TRG%.$$$
 
 del *.map
-
+del test2d.d88
+udostool test2d.d88 -2D
 udostool test2d.d88 ipl.bin -IPL
 udostool test2d.d88 subsys.bin -SUB
 udostool test2d.d88 iosys.bin -SYS
