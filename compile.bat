bdc961bec371c7dd2e640a0bed777a8108cc271b
Kinoppoid <kinoppoid@gmail.com>
2024-08-02 21:16:48 +0900

    The Macroassembler AS　のWin32版にコマンド名をあわせる

--- compile.bat
+++ compile.bat
@@ -1,17 +1,17 @@
 set TRG=ipl
-asw -cpu z80undoc -g map %TRG%.z80
+asl -cpu z80undoc -g map %TRG%.z80
 p2bin %TRG%.p -k -l 0 -r $-$
 
 set TRG=subsys
-asw -cpu z80undoc -g map %TRG%.z80
+asl -cpu z80undoc -g map %TRG%.z80
 p2bin %TRG%.p -k -l 0 -r $-$
 
 set TRG=iosys
-asw -cpu z80undoc -g map %TRG%.z80
+asl -cpu z80undoc -g map %TRG%.z80
 p2bin %TRG%.p -k -l 0 -r $-$
 
 set TRG=$8400
-asw -cpu z80undoc -g map %TRG%.z80
+asl -cpu z80undoc -g map %TRG%.z80
 p2bin %TRG%.p -l 0 -r $-$
 move /Y %TRG%.bin .\file\%TRG%.$$$
 
